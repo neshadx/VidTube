3f3130a845985494985771187738c9c7793a5267
neshadx <neshadimagify@gmail.com>
2025-04-05 19:40:38 +0600

    added some code

--- script/index.js
+++ script/index.js
@@ -1,12 +1,34 @@
 function loadCategories() {
     fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then(res => res.json())
-    .then((data))
+    .then((data) => displayCategories(data.categories)); 
 }
+
+function loadVideos(){
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((response) => response.json())
+    .then((data) => console.log(data)); 
+}
+
+
+
 function displayCategories (categories) {
-    
+    // get the cointainer
+    const categoryContainer = document.getElementById ("category-container");
+    // loop operation on array of object
+    for (let cat of categories) {
+        console.log(cat);
+        // Create element
+        const categoryDiv = document.createElement("div");
+        categoryDiv.innerHTML=`
+           <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+        `;
+        //Append the element
+        categoryContainer.append(categoryDiv);
+    }
 }
 
 
 
+loadCategories();
 loadCategories();
