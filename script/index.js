4e0c9e1f6d904c865ef28824d2ff8a4373eff3de
neshadx <neshadimagify@gmail.com>
2025-04-05 20:09:32 +0600

    added some code

--- script/index.js
+++ script/index.js
@@ -7,7 +7,7 @@ function loadCategories() {
 function loadVideos(){
     fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
     .then((response) => response.json())
-    .then((data) => console.log(data)); 
+    .then((data) => displayVideos(data.videos)); 
 }
 
 
@@ -28,7 +28,37 @@ function displayCategories (categories) {
     }
 }
 
+const displayVideos = (videos) => {
+    const videoContainer = document.getElementById ("video-container");
+    
+
+    videos.forEach((video) => {
+    const videoCard = document.createElement("div");
+
+    videoCard.innerHTML=`
+    
+
+    <div class="card bg-base-100 shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+
+    `;
+
+    videoContainer.append(videoCard);
+    })
+};
 
 
 loadCategories();
-loadCategories();
+loadVideos();
